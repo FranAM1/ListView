3d1f10b8e3ea571f7bf9d3c7cea1fb5cee9f47ba
FranAM1 <falmansamartinez@cifpfbmoll.eu>
2023-12-15 17:11:36 +0100

    url internet

--- app/src/main/java/com/example/listview/InternetURL.java
+++ app/src/main/java/com/example/listview/InternetURL.java
@@ -2,7 +2,13 @@ package com.example.listview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InternetURL extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class InternetURL extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internet_url);
+
+        Button send = findViewById(R.id.ok);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText entry = findViewById(R.id.entry);
+                String url = (String) entry.getText().toString();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
     }
 }
